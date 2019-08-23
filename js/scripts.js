a0783ce511ca21f03fe5823d689a837fa294601b
Arthur-Ochieng <johnarthur016@gmail.com>
2019-08-23 11:13:26 +0300

    updated development icon and icon to toggle

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,12 @@ $(document).ready(function() {
 });
 });
 $(document).ready(function() {
-    $("div#design").click(function() {
-        $("div#content").toggle();
-        $("div#design").toggle();
-    });
-    $("div#content").click(function() {
-       $("div#content").toggle();
-       $("div#design").toggle();
+  $("div#dev").click(function() {
+    $("div#dev-content").toggle();
+    $("div#dev").toggle();
+});
+  $("div#dev-content").click(function() {
+    $("div#dev-content").toggle();
+    $("div#dev").toggle();
 });
 });
