c0a82db27a76629687e741c60626c7cf2bcdf5c5
Arthur-Ochieng <johnarthur016@gmail.com>
2019-08-23 11:02:32 +0300

    added development icon

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,13 @@ $(document).ready(function() {
     $("div#design").toggle();
 });
 });
+$(document).ready(function() {
+    $("div#design").click(function() {
+        $("div#content").toggle();
+        $("div#design").toggle();
+    });
+    $("div#content").click(function() {
+       $("div#content").toggle();
+       $("div#design").toggle();
+});
+});
