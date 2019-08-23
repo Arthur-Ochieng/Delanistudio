bc38744dea8a82229e59ffe2509893eed18a2971
Arthur-Ochieng <johnarthur016@gmail.com>
2019-08-23 11:00:51 +0300

    add service section

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+ $("div#design").click(function() {
+     $("div#content").toggle();
+     $("div#design").toggle();
+ });
+ $("div#content").click(function() {
+    $("div#content").toggle();
+    $("div#design").toggle();
+});
+});
