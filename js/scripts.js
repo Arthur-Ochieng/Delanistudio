ac27030ff8cf38c0c58ecbd25f61bad4dfdd941c
Arthur-Ochieng <johnarthur016@gmail.com>
2019-08-26 08:34:06 +0300

    Added dialog box

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,30 @@ $(document).ready(function() {
  $("div#design").click(function() {
      $("div#content").toggle();
      $("div#design").toggle();
- });
+  });
  $("div#content").click(function() {
     $("div#content").toggle();
     $("div#design").toggle();
-});
-});
-$(document).ready(function() {
+  });
   $("div#dev").click(function() {
     $("div#dev-content").toggle();
     $("div#dev").toggle();
-});
+  });
   $("div#dev-content").click(function() {
     $("div#dev-content").toggle();
     $("div#dev").toggle();
-});
-});
-$(document).ready(function() {
+  });
   $("div#product").click(function() {
     $("div#prod-content").toggle();
     $("div#product").toggle();
-});
+  });
   $("div#prod-content").click(function() {
     $("div#prod-content").toggle();
    $("div#product").toggle();
+  });
+  $("form").click(function() {
+      var name= $("input#name").val();
+      alert("Hello  "+name+" Thank you for contacting us");
+      event.preventDefault();
+  });
 });
-});
-var name = document.getElementByid
