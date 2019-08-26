2c813e30d13b2f454a6bcc149f0ccf2b84622888
Arthur-Ochieng <johnarthur016@gmail.com>
2019-08-26 09:34:27 +0300

    added hovering effect using jquery

--- js/scripts.js
+++ js/scripts.js
@@ -28,4 +28,44 @@ $(document).ready(function() {
       alert("Hello  "+name+" Thank you for contacting us");
       event.preventDefault();
   });
+  $("#photo1").mouseover(function() {
+      $("#text1").show();
+    }).mouseout(function() {
+        $("#text1").hide();
+    });
+  $("#photo2").mouseover(function() {
+      $("#text2").show();
+    }).mouseout(function() {
+        $("#text2").hide();
+    });
+  $("#photo3").mouseover(function() {
+      $("#text3").show();
+    }).mouseout(function() {
+        $("#text3").hide();
+    });
+  $("#photo4").mouseover(function() {
+      $("#text4").show();
+    }).mouseout(function() {
+        $("#text4").hide();
+    });
+  $("#photo5").mouseover(function() {
+      $("#text5").show();
+    }).mouseout(function() {
+        $("#text5").hide();
+    });
+  $("#photo6").mouseover(function() {
+      $("#text6").show();
+    }).mouseout(function() {
+        $("#text6").hide();
+    });
+  $("#photo7").mouseover(function() {
+      $("#text7").show();
+    }).mouseout(function() {
+        $("#text7").hide();
+    });
+  $("#photo8").mouseover(function() {
+      $("#text8").show();
+    }).mouseout(function() {
+        $("#text8").hide();
+    });
 });
